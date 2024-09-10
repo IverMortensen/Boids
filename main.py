d617fd70a3d7652a7ba6ed58efbe2ad8d4bb5719
Solcelle <iver.mortensen@gmail.com>
2024-09-10 23:09:43 +0200

    Implemented alignment rule

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 660
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-NUM_BOIDS = 40
+NUM_BOIDS = 60
 BOID_COLOR = (200, 50, 200)
 
 AVOIDANCE_FACTOR = 0.001
+ALIGNMENT_FACTOR = 0.05
 
 # Set the title of the window
 pygame.display.set_caption("Boids")
@@ -35,7 +36,10 @@ class Vector():
         self.y = y
         
     def __add__(self, other):
-        return Vector(self.x + other.x, self.y + other.y)
+        otherType = type(other)
+        if otherType == Vector:
+            return Vector(self.x + other.x, self.y + other.y)
+        return Vector(self.x + other, self.y + other)
     
     def __mul__(self, other):
         otherType = type(other)
@@ -56,43 +60,74 @@ class Vector():
 class Boid():
     def __init__(self, position: Vector, angle: float):
         self.position = position
-        self.velocity = Vector(random.randint(0, 10) * 0.1, random.randint(0, 10) * 0.1)
+        self.velocity = Vector(random.randint(0, 40) - 20, random.randint(0, 40) - 20)
         self.acceleration = Vector(0, 0)
         self.accelerationRate = 0.01
-        self.magnitudeMaxVelocity = 10
+        self.magnitudeMaxVelocity = 20
         self.angle = angle
         self.rotationSpeed = 5
         self.color = BOID_COLOR
         self.size = 10
 
-        self.avoidRadius = 60
-        self.attractRadius = 120
+        self.avoidDistance = 40
+        self.viewDistance = 100
 
-    def Avoid(self, others: list):
-        avoidaceVector = Vector(0, 0)
+    def Avoid(self, other, avoidVector: Vector, distance):
+        """
+        Sets the avoidence vector away from the other boid
+        if they are within the avoidance distance.
+        """
+        if distance < self.avoidDistance:
+            avoidVector.x += self.position.x - other.position.x
+            avoidVector.y += self.position.y - other.position.y
 
-        # Set the avoidance vector direction away
-        # from the collected direction of nearby boids
+    def Align(self, other, alignVector: Vector):
+        """
+        Sets the alignment vector towards the other boid.
+        """
+        alignVector.x += other.velocity.x
+        alignVector.y += other.velocity.y
+
+    def Update(self, others: list):
+        avoidVector = Vector(0, 0)
+        alignVector = Vector(0, 0)
+        neighbours = 0  # Number of boids within view
+
+        # Find the boids within view
         for other in others:
             distance = self.position.Distance(other.position)
-            if distance < self.avoidRadius:
-                avoidaceVector.x += self.position.x - other.position.x
-                avoidaceVector.y += self.position.y - other.position.y
+            if distance < self.viewDistance:
 
-        # Set the acceleration to the avoidance vector
-        # scaled by the avoidance factor
-        self.acceleration = avoidaceVector * AVOIDANCE_FACTOR
+                # Apply the rules
+                self.Avoid(other, avoidVector, distance)
+                self.Align(other, alignVector)
+
+                # Update number of neighbors
+                neighbours += 1
+
+        # Add the scaled avoidance vector to the acceleration
+        self.acceleration += avoidVector * AVOIDANCE_FACTOR
+
+        # Set the accumulated alignment vector to the
+        # average possition of neighboring boids
+        alignVector.x = alignVector.x / neighbours
+        alignVector.y = alignVector.y / neighbours
+
+        # Add the scaled alignment vector to the acceleration
+        self.acceleration.x += (alignVector.x - self.acceleration.x) * ALIGNMENT_FACTOR
+        self.acceleration.y += (alignVector.y - self.acceleration.y) * ALIGNMENT_FACTOR
 
     def Move(self):
         # Update the velocity based on the acceleration
         self.velocity = self.velocity + self.acceleration
+        self.acceleration.x = self.acceleration.y = 0 # Reset acceleration
 
         # If the new velocity surpasses the max velocity
         if self.velocity.Magnitude() > self.magnitudeMaxVelocity:
             # Set the magnitude of the velocity to the magnitude of the max velocity
             self.velocity = self.velocity.Normalize() * self.magnitudeMaxVelocity
 
-        # Calculate the direction of the boid is moving
+        # Set the direction of the boid to the direction that its moving
         self.angle = math.atan2(self.velocity.y, self.velocity.x)
 
         # Update position based on velocity
@@ -140,7 +175,7 @@ def main():
         screen.fill(BACKGROUND)  # Clear the screen
 
         for boid in boids:
-            boid.Avoid(boids)
+            boid.Update(boids)
             boid.Move()
             boid.Draw(screen)
 
